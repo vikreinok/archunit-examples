3505f20d6f88441089f638fe1fbdf83ad784e82a
Viktor Reinok <vikreinok@gmail.com>
2024-01-13 23:36:29 +0100

    initial commit

---
+++ src/test/java/ch/blog/archunit/ArchunitApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.blog.archunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
