33792b110d58a5008546d9c30525dcc6ba7b012b
Viktor Reinok <vikreinok@gmail.com>
2024-01-28 22:11:38 +0100

    add general tests

---
+++ src/test/java/ch/blog/archunit/GeneralArchUnitTest.java
@@ -0,0 +1,18 @@
+package ch.blog.archunit;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.GeneralCodingRules;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+@AnalyzeClasses(packages = {"ch.blog.archunit"})
+public class GeneralArchUnitTest {
+
+
+    @ArchTest
+    public static final ArchRule should_not_use_strd_streams = noClasses().should(GeneralCodingRules.ACCESS_STANDARD_STREAMS);
+    public static final ArchRule should_not_use_joda_time = noClasses().should(GeneralCodingRules.USE_JODATIME);
+
+}
